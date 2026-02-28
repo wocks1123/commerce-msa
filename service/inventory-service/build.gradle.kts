d42da859aa5bb91b2d24285ea4d7cb521184368b
wocks1123 <wocks1123@gmail.com>
2026-02-28 15:46:37 +0900

    chore(inventory): 공통모듈 의존성 추가

--- service/inventory-service/build.gradle.kts
+++ service/inventory-service/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    implementation(project(":shared:common"))
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
