f75ccb3217a9708a2a58b4de1b276367fcdf4744
wocks1123 <wocks1123@gmail.com>
2026-03-09 22:34:40 +0900

    refactor(inventory): 중복 이벤트 제거
    - KafkaEvent DTO 제거 : 통합 이벤트 직접 발행

--- service/inventory-service/src/main/java/dev/labs/commerce/inventory/core/inventory/infra/messaging/dto/StockDeductedKafkaEvent.java
+++
@@ -1,9 +0,0 @@
-package dev.labs.commerce.inventory.core.inventory.infra.messaging.dto;
-
-public record StockDeductedKafkaEvent(
-        Long productId,
-        String orderId,
-        int quantity,
-        int remainingQuantity
-) {
-}
