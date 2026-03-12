7ef880cc0b65121910d294acbd66932fcd185cb6
wocks1123 <wocks1123@gmail.com>
2026-03-12 21:13:30 +0900

    fix: compile error

--- service/order-service/src/main/java/dev/labs/commerce/order/core/order/application/usecase/ConfirmStockReservedUseCase.java
+++ service/order-service/src/main/java/dev/labs/commerce/order/core/order/application/usecase/ConfirmStockReservedUseCase.java
@@ -1,6 +1,6 @@
 package dev.labs.commerce.order.core.order.application.usecase;
 
-import dev.labs.commerce.order.core.order.application.usecase.dto.ConfirmStockDeductedCommand;
+import dev.labs.commerce.order.core.order.application.usecase.dto.ConfirmStockReservedCommand;
 import dev.labs.commerce.order.core.order.domain.SalesOrder;
 import dev.labs.commerce.order.core.order.domain.SalesOrderRepository;
 import dev.labs.commerce.order.core.order.domain.error.OrderNotFoundException;
@@ -17,9 +17,9 @@ public class ConfirmStockReservedUseCase {
 
     private final SalesOrderRepository salesOrderRepository;
 
-    public void execute(ConfirmStockDeductedCommand command) {
+    public void execute(ConfirmStockReservedCommand command) {
         SalesOrder order = salesOrderRepository.findById(command.orderId())
                 .orElseThrow(OrderNotFoundException::new);
-        order.confirmStockDeducted(Instant.now());
+        order.confirmStockReserved(Instant.now());
     }
 }
