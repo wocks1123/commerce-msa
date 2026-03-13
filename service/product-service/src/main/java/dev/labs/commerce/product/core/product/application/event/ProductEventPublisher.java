1fe1a395135f433016f240502bebba44a4ed02fa
wocks1123 <wocks1123@gmail.com>
2026-03-13 21:36:54 +0900

    refactor: 불필요 @TransactionalEventListener 제거

--- service/product-service/src/main/java/dev/labs/commerce/product/core/product/application/event/ProductEventPublisher.java
+++ service/product-service/src/main/java/dev/labs/commerce/product/core/product/application/event/ProductEventPublisher.java
@@ -1,7 +1,5 @@
 package dev.labs.commerce.product.core.product.application.event;
 
-import dev.labs.commerce.product.core.product.application.event.ProductRegisteredEvent;
-
 public interface ProductEventPublisher {
 
     void publishProductRegistered(ProductRegisteredEvent event);
