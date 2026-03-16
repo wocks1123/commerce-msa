dbaa6ea764134e74d2447ed6910bc69256259472
wocks1123 <wocks1123@gmail.com>
2026-03-16 22:09:04 +0900

    feat(product): http request bean validation 추가

--- service/product-service/src/main/java/dev/labs/commerce/product/api/http/dto/ChangeProductStatusRequest.java
+++ service/product-service/src/main/java/dev/labs/commerce/product/api/http/dto/ChangeProductStatusRequest.java
@@ -1,8 +1,9 @@
 package dev.labs.commerce.product.api.http.dto;
 
 import dev.labs.commerce.product.core.product.domain.ProductStatus;
+import jakarta.validation.constraints.NotNull;
 
 public record ChangeProductStatusRequest(
-        ProductStatus status
+        @NotNull ProductStatus status
 ) {
 }
--- service/product-service/src/main/java/dev/labs/commerce/product/api/http/dto/ModifyProductRequest.java
+++ service/product-service/src/main/java/dev/labs/commerce/product/api/http/dto/ModifyProductRequest.java
@@ -1,9 +1,13 @@
 package dev.labs.commerce.product.api.http.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record ModifyProductRequest(
-        String productName,
-        Long price,
-        String currency,
+        @NotBlank String productName,
+        @NotNull @Positive Long price,
+        @NotBlank String currency,
         String description
 ) {
 }
