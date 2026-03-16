f7f9428a0492d0e70723605d7121621c01fc5aeb
wocks1123 <wocks1123@gmail.com>
2026-03-16 22:23:11 +0900

    feat(order): http request bean validation 추가

--- service/order-service/src/main/java/dev/labs/commerce/order/api/http/dto/CreateOrderRequest.java
+++ service/order-service/src/main/java/dev/labs/commerce/order/api/http/dto/CreateOrderRequest.java
@@ -1,12 +1,17 @@
 package dev.labs.commerce.order.api.http.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.util.List;
 
 public record CreateOrderRequest(
-        long customerId,
-        String currency,
-        long totalPrice,
-        long totalAmount,
-        List<CreateOrderItemRequest> items
+        @Positive long customerId,
+        @NotBlank String currency,
+        @Positive long totalPrice,
+        @Positive long totalAmount,
+        @NotEmpty List<@Valid CreateOrderItemRequest> items
 ) {
 }
