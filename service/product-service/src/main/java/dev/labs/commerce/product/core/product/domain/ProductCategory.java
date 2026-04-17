68b27fb5b4701bee174c0383368028a89c8a8a60
wocks1123 <wocks1123@gmail.com>
2026-04-17 21:29:36 +0900

    feat(product): ProductCategory enum 추가
    장난감/피규어 판매사 도메인에 맞춰 9개 카테고리로 구성:
    FIGURE, PLUSH, MODEL_KIT, TRADING_CARD, BOARD_GAME, DIECAST, PUZZLE, ACCESSORY, ETC

---
+++ service/product-service/src/main/java/dev/labs/commerce/product/core/product/domain/ProductCategory.java
@@ -0,0 +1,13 @@
+package dev.labs.commerce.product.core.product.domain;
+
+public enum ProductCategory {
+    FIGURE,         // 피규어
+    PLUSH,          // 봉제 인형
+    MODEL_KIT,      // 프라모델/모형 키트
+    TRADING_CARD,   // 트레이딩 카드
+    BOARD_GAME,     // 보드게임
+    DIECAST,        // 다이캐스트
+    PUZZLE,         // 퍼즐
+    ACCESSORY,      // 굿즈/액세서리
+    ETC             // 기타
+}
